2a3907daf2a7fefd3fbf6efdbb98899f69f671a9
Anita <anitah75@hotmail.com>
2021-06-18 23:17:34 +1000

    JS and HTML display Cards changes

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,17 @@ const NewTask = document.querySelector("#newTask");
 NewTask.addEventListener('click', newTask);
 
 function newTask(){
-    document.querySelector("#new-task-form").reset();
+    document.querySelector("#taskForm").reset();
+    validationName.style.display = "none"
+    validationDescription.style.display = "none";
+    validationAssignedTo.style.display = "none";
+    validationDueDate.style.display = "none";
+    validationStatus.style.display = "none";
+    validationName.innerHTML = "none";
+    validationDescription.innerHTML = "none";
+    validationAssignedTo.innerHTML = "none";
+    validationDueDate.innerHTML = "none";
+    validationStatus.innerHTML = "none";
 };
 
 //Validation of the form
@@ -25,78 +35,137 @@ let dueDate = document.querySelector("#dueDate");
 //console.log(dueDate.value);
 let status = document.querySelector("#status");
 //console.log("status: " + status.value);
-let submitTask = document.querySelector("#btnAddTask");
+let submitTask = document.querySelector("#submitTask");
 let validationName = document.querySelector("#validationName");
 let validationDescription = document.querySelector("#validationDescription");
 let validationAssignedTo = document.querySelector("#validationAssignedTo");
 let validationDueDate = document.querySelector("#validationDueDate");
 let validationStatus = document.querySelector("#validationStatus");
 let taskForm = document.querySelector("#taskForm");
+let taskList = document.querySelector("#taskList");
 
-const validFormFieldInput = (event) => {
-    event.preventDefault();
-    let nameOk = false;
-    let descriptionOk = false;
-    let assignOk = false;
-    let dateOk = false;
-    let statusOk = false;
-    // Name input validation
+let nameOk = false;
+let descriptionOk = false;
+let assignOk = false;
+let dateOk = false;
+let statusOk = false;
+
+// Name input validation function
+const nameValidFunc = ()=> {
+    nameOk = false;
     if(taskName.value.length >= 5){
-        validationName.innerHTML = "Looks good!";
-        validationName.style.color = "green";
-        taskName.style.borderColor = "green";
-        nameOk = true;
-    } else {
-        validationName.innerHTML = "Please provide a valid description";
-        validationName.style.color = "red";
-        taskName.style.borderColor = "red";
-    }
-    // Description input validation
+    validationName.style.display = "block";
+    validationName.innerHTML = "Looks good!";
+    validationName.style.color = "green";
+    taskName.style.borderColor = "green";
+    nameOk = true;
+} else {
+    validationName.style.display = "block";
+    validationName.innerHTML = "Please provide a valid description";
+    validationName.style.color = "red";
+    taskName.style.borderColor = "red";
+}};
+taskName.addEventListener("input",nameValidFunc);
+
+ // Description input validation function 
+const descrValidFunc = () => {
+    descriptionOk = false;
     if(description.value.length >= 5){
-        validationDescription.innerHTML = "Looks good!";
-        validationDescription.style.color = "green";
-        description.style.borderColor = "green"
-        descriptionOk = true;
-    } else {
-        validationDescription.innerHTML = "Please provide a valid description";
-        validationDescription.style.color = "red";
-        description.style.borderColor = "red";
-    }
-    // assignTo validation
+    validationDescription.style.display = "block";
+    validationDescription.innerHTML = "Looks good!";
+    validationDescription.style.color = "green";
+    description.style.borderColor = "green"
+    descriptionOk = true;
+} else {
+    validationDescription.style.display = "block";
+    validationDescription.innerHTML = "Please provide a valid description";
+    validationDescription.style.color = "red";
+    description.style.borderColor = "red";
+}};
+description.addEventListener("input",descrValidFunc);
+
+// assignTo validation function
+const assignValidFunc = () =>{
+    assignOk = false;
     if(assignedTo.value.length >= 5){
+        validationAssignedTo.style.display = "block";
+        validationAssignedTo.style.display = "block";
         validationAssignedTo.innerHTML = "Looks good!";
         validationAssignedTo.style.color = "green";
         assignedTo.style.borderColor = "green";
         assignOk = true;
     } else {
+        validationAssignedTo.style.display = "block";
         validationAssignedTo.innerHTML = "Please provide a valid description";
         validationAssignedTo.style.color = "red";
         assignedTo.style.borderColor = "red";
     }
+};
+assignedTo.addEventListener("input",assignValidFunc);
 
-    //dueDate validation
+//dueDate validation function
+const dateValidfunc = () => {
+    dateOk = false;
     if(dueDate.value !== "") {
+        let date = new Date(dueDate.value);
+        date.setHours(0,0,0,0);
+        let currentDate = new Date();
+        currentDate.setHours(0,0,0,0);
+        if (currentDate <= date ){
+        validationDueDate.style.display = "block";
         validationDueDate.innerHTML = "Looks good!";
         validationDueDate.style.color = "green";
         dueDate.style.borderColor = "green";
         dateOk = true;
+        }else {
+            validationDueDate.style.display = "block";
+            validationDueDate.innerHTML = "The Date needs to be greater or equal than Today's Date!";
+            validationDueDate.style.color = "red";
+            dueDate.style.borderColor = "red";
+        }
     } else {
+        validationDueDate.style.display = "block";
         validationDueDate.innerHTML = "Please provide a valid description";
         validationDueDate.style.color = "red";
         dueDate.style.borderColor = "red";
-    }
-    // status validation
+        }
+};
+dueDate.addEventListener("input",dateValidfunc);
+
+// status validation function
+const statusValidFunction = () => {
+    statusOk = false;
     if(status.value !== "Select") {
+        validationStatus.style.display = "block";
         validationStatus.innerHTML = "Looks good!";
         validationStatus.style.color = "green";
         status.style.borderColor = "green";
         statusOk = true;
     } else {
-        event.preventDefault();
+        validationStatus.style.display = "block";
         validationStatus.innerHTML = "Please provide a valid description";
         validationStatus.style.color = "red";
         status.style.borderColor = "red";
     }
+};
+status.addEventListener("input",statusValidFunction);
+
+const validFormFieldInput = (event) => {
+    event.preventDefault();
+    // Name input validation
+    nameValidFunc();
+    
+    // Description input validation
+    descrValidFunc();
+
+    // assignTo validation
+    assignValidFunc();
+
+    //dueDate validation
+    dateValidfunc();
+   
+    // status validation
+    statusValidFunction();
     
     // add task step
     if(nameOk && descriptionOk && assignOk && dateOk && statusOk){
@@ -125,19 +194,38 @@ const validFormFieldInput = (event) => {
         validationDueDate.style.display = "none";
         dueDate.style.borderColor = "#ced4da";
 
-        // reset satus
+        // reset status
         status.value = "Select";
         validationStatus.style.display = "none";
         status.style.borderColor = "#ced4da";
 
         //console.log(taskManager.render());
-        taskManager.render();
-    }
+        
+    } 
+    taskManager.render();
 };
 
 
 taskForm.addEventListener("submit", validFormFieldInput);
 
+
 //test to create a task HTML 
 // let taskHtml = createTaskHtml("Cooking","dinner for tonight", "Ana", "11/06/2021", "Pending");
 // console.log(taskHtml);
+
+
+taskList.addEventListener("click", (event) =>{
+    if (event.target.classList.contains("done-button")){
+        //console.log(event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
+        const parentTask = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        //console.log(parentTask.dataset.taskId);
+        const taskId = Number(parentTask.dataset.taskId);
+        //console.log(taskId);
+        const task = taskManager.getTaskById(taskId);
+        //console.log(task);
+        task.status = "Done";
+
+        taskManager.render();
+        //event.target.style.display = "none";
+    }   
+});
--- JS/taskManager.js
+++ JS/taskManager.js
@@ -1,58 +1,82 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status)  => {
-    const html = `<li class="list-group-item list-border">
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status)  => {
+    let visibility;
+    if(status === "Done"){
+      visibility = "invisible";
+    }else{
+      visibility = "visible";
+    }
+    const html = `
+    <li class="list-group-item list-border" data-task-id="${id}">
     <div class="card cardstyle">
       <div class="card-body">
         <h3 class="card-title">${name}</h3>
         <p class="card-text">${description}</p>
         <h6 class="card-subtitle mb-2 mt-2">Assigned To: <span class="h5 strong">${assignedTo}</span></h6>
         <h6 class="card-subtitle my-3">Date: <span class="strong">${dueDate}</span></h6>
-        <div class="d-flex align-items-baseline justify-content-between">
-          <h6 class="card-subtitle my-3 mt-5">Status: <span class="strong">${status}</span></h6>
-          <button class="btn btn-lg btn-trash"><i class="fa fa-trash"></i></button>
+        <div class="align-items-baseline justify-content-between">
+          <h6 class="card-subtitle my-3 mt-4">Status: <span class="strong">${status}</span></h6>
+          <div class="container">
+            <div class="row row-cols-1 row-cols-md-4 justify-content-center">
+              <button class="col-4 btn btn-outline-success done-button ${visibility} mx-5"><i class="fa fa-thumbs-o-up "></i>Done</button> 
+              <button class="col-4 btn btn-outline-danger mx-5"><i class="fa fa-trash"></i>Delete</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </li>`;
-
+  
   return html;
-}
-
-class TaskManager {
-    constructor(currentId = 0){
-        this.tasks = [];
-        this.currentId = currentId;
-    }
-    addTask(name, description, assignedTo, dueDate, status) {
-        const newTask = {
-            id: this.currentId++,
-            name: name,
-            description: description,
-            assignedTo: assignedTo,
-            dueDate: dueDate,
-            status: status
-        };
-        this.tasks.push({ newTask });
-    }
-    render(){
-        const tasksHtmlList = [];
+  }
+  
+  class TaskManager {
+      constructor(currentId = 0){
+          this.tasks = [];
+          this.currentId = currentId;
+      }
+      addTask(name, description, assignedTo, dueDate, status) {
+          const newTask = {
+              id: this.currentId++,
+              name: name,
+              description: description,
+              assignedTo: assignedTo,
+              dueDate: dueDate,
+              status: status
+          };
+          this.tasks.push(newTask);
+      }
+      render(){
+          const tasksHtmlList = [];
+          for(let i = 0; i < this.tasks.length; i++){
+            const renderTask = this.tasks[i];
+            //console.log(renderTask);
+            const date = new Date(renderTask.dueDate);
+            //console.log(date);
+            const formattedDate = ("0" + date.getDate()).slice(-2) + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
+            //console.log(formattedDate);
+            const taskHtml = createTaskHtml(
+              renderTask.id,
+              renderTask.name, 
+              renderTask.description, 
+              renderTask.assignedTo, 
+              formattedDate, 
+              renderTask.status);
+            tasksHtmlList.push(taskHtml);
+            //console.log(taskHtml);
+          }
+          const taskHtml = tasksHtmlList.join("\n");
+          const taskList = document.querySelector("#taskList");
+          taskList.innerHTML = taskHtml;
+      }
+  
+      getTaskById(taskId){
+        let foundTask;
         for(let i = 0; i < this.tasks.length; i++){
-          const renderTask = this.tasks[i];
-          //console.log(renderTask);
-          const date = new Date(renderTask.newTask.dueDate);
-          //console.log(date);
-          const formattedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-          //console.log(formattedDate);
-          const taskHtml = createTaskHtml(
-            renderTask.newTask.name, 
-            renderTask.newTask.description, 
-            renderTask.newTask.assignedTo, 
-            formattedDate, 
-            renderTask.newTask.status);
-          tasksHtmlList.push(taskHtml);
-          //console.log(taskHtml);
+          const task = this.tasks[i];
+          if(task.id === taskId){
+            foundTask = task;
+          }
         }
-        const taskHtml = tasksHtmlList.join("\n");
-        const taskList = document.querySelector("#displayTask");
-        taskList.innerHTML = taskHtml;
-    }
-};
+        return foundTask;
+      }
+  };
